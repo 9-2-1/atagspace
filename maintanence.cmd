72c245d54dac48478fa95f3c885087359c178d32
arigi <arigi@arigi.top>
2025-11-22 22:30:41 +0800

    fix: 将扩展标签命令参数从 -c 改为 -s
    修改维护脚本中的参数，使用 -s 替代 -c 以正确执行扩展标签操作

--- maintanence.cmd
+++ maintanence.cmd
@@ -1,5 +1,5 @@
 call conda activate atagspace
 python app.py tagfile updatenew
-python app.py extension totag -c
+python app.py extension totag -s
 python app.py extension sorttag
 if not "%~1" == "nopause" pause
