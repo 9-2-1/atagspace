11122aacfcc5374b439eff11e3d7242c0c831a24
arigi <arigi@arigi.top>
2025-11-23 16:11:50 +0800

    feat: 添加自动生成moverule.txt的脚本并更新规则
    添加makerule.py脚本用于自动生成moverule.txt文件，包含图片和其他文件的分类规则。
    同时更新moverule.txt中的路径规则，修正部分分类路径并优化规则格式。

---
+++ makerule.py
@@ -0,0 +1,71 @@
+import sys
+
+with open("moverule.txt", "w", encoding="utf-8") as f:
+    stdout = sys.stdout
+    sys.stdout = f
+    tags = (
+        "常用-负面 负面-争议话题 负面-推广 负面-观猴 负面-锤人 高中-负面 负面事件 事件 纪念-CTF"
+        " 纪念-Scratch 纪念-Scratch-负面 纪念-种子杯 纪念-其他编程 大学 高中 娱乐-动画 娱乐-游戏"
+        " 娱乐-动画推荐 娱乐-游戏推荐 娱乐-漫画推荐 娱乐-故事推荐 应用推荐 娱乐-up 主题"
+        " 主题-计算机 搞笑合集 应用 特别 字体"
+    ).split(" ")
+
+    print(
+        """\
++!重要文件 = /OneDrive/重要文件
++!难下载 = /下载/难下载
+~常用 = /OneDrive/常用/{常用}
+"""
+    )
+
+    print(
+        """\
+# Pictures / Videos
+"""
+    )
+
+    PICTURES = "( *.jpg | *.png | *.jpeg | *.gif | *.mp4 )"
+
+    for tag in tags:
+        print(f"{PICTURES} ~{tag} = /Saved Pictures/{tag}/{{{tag}}}")
+    print()
+
+    print(
+        f"""
+{PICTURES} ( +!优先-1 | +!奇葩-1 | +!搞笑-1 | +!抽象-1 | +!优秀-1 | +!封神-1 ) = /Saved Pictures/!精选/
+"""
+    )
+
+    for tag in "!待分类 !待更新 !待阅读 !持续关注".split(" "):
+        print(f"{PICTURES} +{tag} = /Saved Pictures/{tag}")
+    print()
+
+    print(
+        """\
+# Other
+"""
+    )
+
+    for tag in tags:
+        print(f"~{tag} = /下载/{tag}/{{{tag}}}")
+    print()
+
+    print(
+        f"""
+( +!优先-1 | +!奇葩-1 | +!搞笑-1 | +!抽象-1 | +!优秀-1 | +!封神-1 ) = /下载/!精选/
+"""
+    )
+
+    for tag in "!待分类 !待更新 !待阅读 !持续关注".split(" "):
+        print(f"+{tag} = /下载/{tag}")
+    print()
+
+    print(
+        """\
++!不安全 = /下载/!不安全
++!不重要 = /下载/!不重要
++!准备删除 = /下载/!准备删除
+"""
+    )
+
+    sys.stdout = stdout
