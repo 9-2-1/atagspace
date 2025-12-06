0a7d5922b0d41ea5318ad30b84e4acc67aea65f4
arigi <arigi@arigi.top>
2025-12-06 20:53:06 +0800

    不需要额外安装less

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { less } from 'svelte-preprocess-less';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [svelte({ preprocess: [less()] })],
+  plugins: [svelte()],
   server: {
     proxy: { '/api': { target: 'http://localhost:4590', changeOrigin: true, secure: false } },
   },
